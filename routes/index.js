3616c7ba8de4c0cb5930f9b71eb2bc2e684ab5f8
Victor Medina <mgvictor7@gmail.com>
2015-07-20 22:39:10 +0100

    Añadido el autoload

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,10 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.param('quizId', quizController.load); //autoload :quizId
+
 router.get('/quizes', quizController.index);
-
 router.get('/quizes/:quizId(\\d+)', quizController.show);
-
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 router.get('/author', function(req, res) {
