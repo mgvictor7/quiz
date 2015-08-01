1b115856037b3200b83201ed868d4b9fd7d09d9f
Victor Medina <mgvictor7@gmail.com>
2015-08-02 00:11:54 +0100

    Añadir a cada pregunta un índice temático

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -55,8 +55,9 @@ exports.answer = function(req, res) {
 exports.new = function(req, res) {
 	var quiz = models.Quiz.build(
 		{
-			pregunta: "pregunta",
-			respuesta: "respuesta"
+			pregunta: "",
+			respuesta: "",
+			tema: ""
 		}
 	);
 	res.render('quizes/new', {quiz: quiz, errors: []});
@@ -73,7 +74,7 @@ exports.create = function(req, res) {
 				res.render('quizes/new', {quiz: quiz, errors: err.errors});
 			} else {
 				quiz
-				.save({fields: ["pregunta", "respuesta"]})
+				.save({fields: ["pregunta", "respuesta", "tema"]})
 				.then(function() {
 					res.redirect('/quizes');	
 				});
@@ -94,6 +95,7 @@ exports.update = function(req, res) {
 
 	req.quiz.pregunta = req.body.quiz.pregunta;
 	req.quiz.respuesta = req.body.quiz.respuesta;
+	req.quiz.tema = req.body.quiz.tema;
 
 	req.quiz
 	.validate()
@@ -103,7 +105,7 @@ exports.update = function(req, res) {
 				res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
 			} else {
 				req.quiz
-				.save({fields: ["pregunta", "respuesta"]})
+				.save({fields: ["pregunta", "respuesta", "tema"]})
 				.then(function() {
 					res.redirect('/quizes');	
 				});
