205ee30759b8d24bd0be4811702e7ce820fd3178
Victor Medina <mgvictor7@gmail.com>
2015-08-02 12:36:05 +0100

    Añadida la moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ router.get('/', function(req, res) {
 
 // Autoload de comandos :quizId
 router.param('quizId', quizController.load);
+router.param('commentId', commentController.load);
 
 // Rutas de sesión
 router.get('/login', sessionController.new);
@@ -33,6 +34,7 @@ router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizCont
 // Rutas para los comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 router.get('/author', function(req, res) {
 	res.render('author',{errors: []});
